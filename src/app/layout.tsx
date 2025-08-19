a28989d32928f0b2cdba7b6d04fcc28f3191c281
Abhishek Yadav <abhishekyadav8952@gmail.com>
2025-08-19 19:08:56 +0000

    make the all text style attractive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,19 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { Inter, Space_Grotesk } from 'next/font/google';
+import { cn } from '@/lib/utils';
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-headline',
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-body',
+});
+
 
 export const metadata: Metadata = {
   title: 'Abhishek Yadav Portfolio',
@@ -14,12 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased animated-gradient">
+      <body className={cn("font-body antialiased animated-gradient", spaceGrotesk.variable, inter.variable)}>
         {children}
         <Toaster />
       </body>
